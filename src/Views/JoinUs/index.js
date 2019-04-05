6b8060c73703377e41bbd686c56db238176da58d
John Ogenyi <johnogenyi4real@gmail.com>
2019-04-05 16:40:28 +0100

    updated the signup page and landing page

--- src/Views/JoinUs/index.js
+++ src/Views/JoinUs/index.js
@@ -1,13 +1,66 @@
 import React, { Component } from "react";
- 
+import {
+  Container, Col, Form,
+  FormGroup, Label, Input,
+  Button,
+} from 'reactstrap';
+import "./joinUs.css";
 class JoinUs extends Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e) {
+    alert('The value is: ' + this.input.value);
+    e.preventDefault();
+  }
   render() {
     return (
-      <div>
-        <h2>Join Us</h2>
+      <div className="join">
+        <Container className="App">
+          <h2>Join Us</h2>
+          <Form className="form">
+            <Col>
+              <FormGroup>
+                <Label>Your name</Label>
+                <Input
+                  icon="user"
+                  type="name"
+                  name="name"
+                  id="exampleName"
+                  placeholder="your full name"
+                />
+              </FormGroup>
+            </Col>
+            <Col>
+              <FormGroup>
+                <Label for="examplePassword">Password</Label>
+                <Input
+                  type="password"
+                  name="password"
+                  id="examplePassword"
+                  placeholder="********"
+                />
+              </FormGroup>
+            </Col>
+            <Col>
+              <FormGroup>
+                <Label for="examplePhonenumber">Phone Number</Label>
+                <Input
+                  type="phonenumber"
+                  name="phonenumber"
+                  id="examplePhonenumber"
+                  placeholder="081********"
+                />
+              </FormGroup>
+            </Col>
+            <Button>Submit</Button>
+          </Form>
+        </Container>
       </div>
     );
   }
 }
- 
+
 export default JoinUs;
--- src/Views/Landing/index.js
+++ src/Views/Landing/index.js
@@ -1,15 +1,30 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import "./landing.css";
+import fblogo from '../../assets/fblogo.png'
+import iglogo from '../../assets/iglogo.png'
+import twlogo from '../../assets/twlogo.png'
 import logo from '../../assets/logo.png'
+// import {FacebookShareButton, TwitterShareButton, FacebookIcon} from 'react-share';
+
+
 
 class Landing extends Component {
   render() {
     return (
       <div className="landing">
-      <div className="logo-container" >
-        <img src={logo} alt="qr8tors_logo" />
-      </div>
+        {/* <FacebookIcon size={32} round={true} /> */}
+        <div className="socialLogo">
+          <span>
+            <a href="#"><img src={fblogo} alt="fb_logo" style={{ maxHeight: '30px' }} /> </a>
+            <a href="#"><img src={iglogo} alt="ig_logo" style={{ maxHeight: '30px' }} /> </a>
+            <a href="#"><img src={twlogo} alt="tw_logo" style={{ maxHeight: '30px' }} /> </a>
+          </span>
+        </div>
+
+        <div className="logo-container" >
+          <img src={logo} alt="qr8tors_logo" />
+        </div>
         <div className="button-container" >
           <button>
             <Link to="/join-us">JoinUs</Link>
@@ -19,6 +34,7 @@ class Landing extends Component {
           </button>
         </div>
       </div>
+
     );
   }
 }
