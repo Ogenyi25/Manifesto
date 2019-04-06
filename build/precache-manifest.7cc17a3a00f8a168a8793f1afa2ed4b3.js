7b6879d4bfffdf05bb5e475cc9f718b9afbfcbb8
Balanced <adebalanced02@gmail.com>
2019-04-06 15:16:39 +0100

    build dev heroku

---
+++ build/precache-manifest.7cc17a3a00f8a168a8793f1afa2ed4b3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "190f937ceb681a70209e0da15cb29457",
+    "url": "/static/media/iglogo.190f937c.png"
+  },
+  {
+    "revision": "b11351318084d54e5c9a",
+    "url": "/static/css/main.eb2986d9.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "eb357318b910974d9497",
+    "url": "/static/js/2.b8abcbeb.chunk.js"
+  },
+  {
+    "revision": "29f4420f6d8b86a708a988692e7b2db4",
+    "url": "/static/media/fblogo.29f4420f.png"
+  },
+  {
+    "revision": "b11351318084d54e5c9a",
+    "url": "/static/js/main.5047dc46.chunk.js"
+  },
+  {
+    "revision": "b173b7165555888b9512bd5576b4c751",
+    "url": "/static/media/twlogo.b173b716.png"
+  },
+  {
+    "revision": "e179a723c518f8e2c51a319d93a7670b",
+    "url": "/static/media/logo.e179a723.png"
+  },
+  {
+    "revision": "8fc622c3a2e2d992ec059cca61e3dfc0",
+    "url": "/static/media/impact.8fc622c3.ttf"
+  },
+  {
+    "revision": "80eaddf32645d5c8b6590814550c44b1",
+    "url": "/static/media/Verdana Ref.80eaddf3.ttf"
+  },
+  {
+    "revision": "fb8efbbf684571e026eab63438fd7060",
+    "url": "/index.html"
+  }
+];
